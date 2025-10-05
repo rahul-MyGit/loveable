d36d68b264bd7596f76e8a99461455ad3a8c1b84
Rahul <rahul.mymail1@gmail.com>
2025-10-05 22:07:43 +0530

    conf prisma

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import { Button } from "@/components/ui/button";
+import Component from "@/components/component";
 
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center h-screen bg-green-500 text-4xl">
       Hello World
       <Button>Click me</Button>
+      <Component />
     </div>
   );
 }
