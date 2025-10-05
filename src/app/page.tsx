298f346ff42854e2a11d7050b797c6f907e99c21
Rahul <rahul.mymail1@gmail.com>
2025-10-05 22:37:49 +0530

    feat: trpc

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,13 @@
 import { Button } from "@/components/ui/button";
-import Component from "@/components/component";
+import { useTRPC } from "@/trpc/client";
 
 export default function Home() {
+  const trpc = useTRPC();
+  trpc.askAI.queryOptions({ text: "World" });
   return (
     <div className="flex flex-col items-center justify-center h-screen bg-green-500 text-4xl">
       Hello World
       <Button>Click me</Button>
-      <Component />
     </div>
   );
 }
